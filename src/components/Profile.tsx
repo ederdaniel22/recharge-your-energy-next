873549438a0d09b2f67afbfe80061948a5788ab2
EDER DANIEL DOMINGUES <60990935+ederdaniel22@users.noreply.github.com>
2021-02-24 22:21:29 -0400

    Criação e estilização dos desafios

---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/components/Profile.module.css';
+export function Profile(){
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/ederdaniel22.png" alt="Eder Daniel Domingues" />
+                <div>
+
+                    <strong>
+                         Eder Daniel Domingues
+                    </strong>
+
+                    <p>
+                        <img src="icons/level.svg" alt="Level" />
+                        Level 1
+                        </p>
+                </div>
+            
+        </div>
+       
+    );
+}
